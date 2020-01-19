4218b9cfbf35fc4cd37722b0d3c914ae6b16a0c8
Lloydsama <emlolydcastro@gmail.com>
2020-01-18 22:49:18 -0700

    added database

--- lambdaFunction.py
+++ lambdaFunction.py
@@ -1,1 +1,10 @@
-""" This is a test """
+p1='What might you consider your biggest strength?'
+p2=' What might you consider your biggest weakness?'
+p3='Give an example of a project you worked on, in which you were dissatisfied with your own work, and what you would do differently.'
+p4='What is your definition of hard work?'
+p5='How do you personally define success?'
+p6='How do you handle stress?'
+p7='What are your passions outside of your career'
+p8='Why did you choose this profession?'
+
+print(p1)
